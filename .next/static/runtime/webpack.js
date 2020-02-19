eb5aaf3bb7e993425ca4e999c3ca973d7a2d5507
epolat <etuna1999@gmail.com>
2020-02-19 20:46:05 +0300

    minor fixes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e0bc3d60c708fcd81efd";
+/******/ 	var hotCurrentHash = "00a4abf3a0824e02231d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
